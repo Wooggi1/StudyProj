6c368603527008a4ca79ad51a583e84ff4ca4ad9
Thiago Silva <thiago2b9s@gmail.com>
2023-07-31 02:46:18 -0300

    eh pika

--- studyApp/models.py
+++ studyApp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Feature:
-    id: int
-    name: str
-    details: str
-    image_path: str
-    is_legal: bool
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.TextField(default='')
+    image_path = models.TextField(default='')
+    is_legal = models.BooleanField(default=True)
+
+class Meta:
+        app_label = 'studyApp'
